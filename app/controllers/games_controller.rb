96454fa53b80420a2e37d98f07d563a581a400c4
Riley Mathews <rileymathews80@gmail.com>
2023-03-20 10:07:47 -0500

    refactored games controller to use lets pattern

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,53 +1,33 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: %i[ show edit update destroy ]
+  let(:games) { Game.ordered }
+  let(:game) { games.find_or_initialize_by(slug: params[:id]) }
 
   # GET /games or /games.json
-  def index
-    @games = Game.ordered
-  end
+  def index; end
 
   # GET /games/1 or /games/1.json
-  def show
-  end
+  def show; end
 
   # GET /games/new
-  def new
-    @game = Game.new
-  end
+  def new; end
 
   # GET /games/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /games or /games.json
   def create
-    @game = Game.new(game_params)
-    @game.created_by = current_user
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to game_url(@game), notice: "Game was successfully created." }
-        format.turbo_stream { flash.now[:notice] = "Game was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
+    game.created_by = current_user
+    save
   end
 
   # PATCH/PUT /games/1 or /games/1.json
   def update
-    respond_to do |format|
-      if @game.update(game_params)
-        format.html { redirect_to game_url(@game), notice: "Game was successfully updated." }
-        format.turbo_stream { flash.now[:notice] = "Game was successfully updated." }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
+    save
   end
 
   # DELETE /games/1 or /games/1.json
   def destroy
-    @game.destroy
+    game.destroy
 
     respond_to do |format|
       format.html { redirect_to games_url, notice: "Game was successfully destroyed." }
@@ -56,13 +36,20 @@ class GamesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Game.find_by(slug: params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def game_params
-      params.require(:game).permit(:name, :uuid, :slug, :created_by_id)
+  def save
+    respond_to do |format|
+      if game.update(game_params)
+        format.html { redirect_to game_url(game), notice: "Game was successfully saved" }
+        format.turbo_stream { flash.now[:notice] = "Game was successively saved." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
+  end
+
+  # Only allow a list of trusted parameters through.
+  def game_params
+    params.require(:game).permit(:name, :uuid, :slug, :created_by_id)
+  end
 end
