b543d7ce26b6cb4487261f6e69c9e82dd934b72e
Riley Mathews <rileymathews80@gmail.com>
2023-03-28 18:07:07 -0500

    delete routes and nav item

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  resources :games, shallow: true do
-    resources :rooms
-    resources :dice do
-      resources :faces, shallow: false
-    end
-  end
   devise_for :users
   root to: "home#index"
 end
