4374828f8245a811837a075ce56831f7312d81d0
Riley Mathews <rileymathews80@gmail.com>
2023-03-29 14:48:40 -0500

    reset dice

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :created_by, class_name: "User"
+  has_many :dice, dependent: :destroy
+
+  validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_140215) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_194207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dice", force: :cascade do |t|
+    t.string "name"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_dice_on_game_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -40,5 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_140215) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dice", "games"
   add_foreign_key "games", "users", column: "created_by_id"
 end
