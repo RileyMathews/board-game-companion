46fd4309733acb3e70186b66ab8aea36436a69be
Riley Mathews <rileymathews80@gmail.com>
2023-04-12 15:51:00 -0500

    add capybara matchers to component specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
+require "view_component/test_helpers"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -78,4 +79,7 @@ RSpec.configure do |config|
   end
 
   config.include Capybara::RSpecMatchers, type: :request
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include ViewComponent::SystemTestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
 end
