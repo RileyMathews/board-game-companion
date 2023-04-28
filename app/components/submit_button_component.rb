447b664e74ccfa97656572f422c29c994eb39d0a
Riley Mathews <rileymathews80@gmail.com>
2023-04-28 14:59:24 -0500

    removed view components

--- app/components/submit_button_component.rb
+++
@@ -1,9 +0,0 @@
-class SubmitButtonComponent < ViewComponent::Base
-  include ButtonBase
-
-  def initialize(text:, variant: :default)
-    @text = text
-    @variant = variant
-    @classes = full_class_list
-  end
-end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
-require "view_component/test_helpers"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -79,7 +78,5 @@ RSpec.configure do |config|
   end
 
   config.include Capybara::RSpecMatchers, type: :request
-  config.include ViewComponent::TestHelpers, type: :component
-  config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
 end
