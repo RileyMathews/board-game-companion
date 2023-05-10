c1caca5167fb99df441f5a7828c0392a83fcfbf5
Riley Mathews <rileymathews80@gmail.com>
2023-05-09 19:20:33 -0500

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,6 @@ Rails.application.routes.draw do
   post "/rooms/join/:join_code", to: "join_room#join_room", as: "join_room"
 
   get "/rooms/:room_id/play", to: "play#index", as: "room_play"
-  post "/rooms/:room_id/roll/:die_id/:number", to: "play#roll", as: "roll_dice"
-  post "/rooms/:room_id/archive_rolls", to: "play#archive_rolls", as: "archive_rolls"
-  post "/play/rolls/:roll_id/archive", to: "play#archive_roll", as: "archive_roll"
-
-  post "/room_resources/:id/increment", to: "room_resources#increment", as: "increment_room_resource"
-  post "/room_resources/:id/decrement", to: "room_resources#decrement", as: "decrement_room_resource"
 
   root to: "home#index"
 end
