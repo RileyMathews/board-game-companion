f9b29c81bc0edc0a824da73d2ed600abc28ecd75
Riley Mathews <rileymathews80@gmail.com>
2023-05-09 19:18:52 -0500

    turbo dice rolls

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -2,24 +2,7 @@ class PlayController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # TODO: see if this can be broken up into components via lazy loaded turbo frames?
-    user_room = UserRoom.find_by(room_id: params[:room_id], user: current_user)
-    authorize! :play, user_room
-    @room = user_room.room
-
-    @dice = @room.game.dice
-    @roll_options = 1..10
-    @all_rolls = Roll.for_room_log(@room)
-  end
-
-  def roll # rubocop:disable Metrics/AbcSize
-    user_room = UserRoom.find_by(room_id: params[:room_id], user: current_user)
-    authorize! :play, user_room
-    @roll = Die.find(params[:die_id]).roll user: current_user, room: user_room.room, number: params[:number]
-
-    respond_to do |format|
-      format.html { redirect_to room_play_url(user_room.room), notice: @roll.summary_string }
-      format.turbo_stream
-    end
+    @room = Room.find params[:room_id]
+    authorize! :play, @room
   end
 end
--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,7 +1,35 @@
 class RollsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room
+
   def index
-    @room = Room.find params[:room_id]
     authorize! :play, @room
     @rolls = @room.rolls.order(created_at: :desc)
+  end
+
+  def new
+    authorize! :play, @room
+    @dice = Die.where(game: @room.game)
+    @roll_options = 1..10
+  end
+
+  def create
+    authorize! :play, @room
+    die = Die.find(create_params[:die_id])
+    @roll = die.roll user: current_user, room: @room, number: create_params[:number]
+
+    respond_to do |format|
+      format.html { redirect_to room_play_url(@room), notice: @roll.summary_string }
+    end
+  end
+
+private
+
+  def set_room
+    @room = Room.find params[:room_id]
+  end
+
+  def create_params
+    params.permit(:die_id, :number)
   end
 end
