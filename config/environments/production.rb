70a274a928d3d85553c6a75436ce223adba8668c
Riley Mathews <rileymathews80@gmail.com>
2023-06-02 09:49:30 -0500

    new fly deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   ActionMailer::Base.smtp_settings = {
     user_name: "apikey", # This is the string literal "apikey", NOT the ID of your API key
-    password: ENV.fetch("SENDGRID_API_KEY", nil),
+    password: Rails.application.credentials.sendgrid_api_key,
     domain: "rileymathews.com",
     address: "smtp.sendgrid.net",
     port: 587,
