084e503a6735c6ab6ab5a8dd4b33da07b90eb914
Riley Mathews <rileymathews80@gmail.com>
2023-06-09 16:04:10 -0500

    started adding stimulus controller

---
+++ app/javascript/controllers/room_resource_form_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="room-resource-form"
+export default class extends Controller {
+  static targets = ["plus_input", "minus_input", "new_value"]
+
+  connect() {
+    
+  }
+
+  plus() {
+
+  }
+
+  minus() {
+
+  }
+}
