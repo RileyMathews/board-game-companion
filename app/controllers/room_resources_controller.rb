9b4a3bf8a9ba3fb5c1082ab72cb98f73a02e9a81
Riley Mathews <rileymathews80@gmail.com>
2023-06-09 17:01:57 -0500

    simplified with only turbo frames

--- app/controllers/room_resources_controller.rb
+++ app/controllers/room_resources_controller.rb
@@ -6,8 +6,6 @@ class RoomResourcesController < ApplicationController
   def index
     authorize! :show, @room
     @room_resources_grouped = @room.grouped_resources_for_player user: current_user
-    @other_players_resources = RoomResource.other_players(@room, current_user)
-                                           .group_by(&:user)
   end
 
   def edit
